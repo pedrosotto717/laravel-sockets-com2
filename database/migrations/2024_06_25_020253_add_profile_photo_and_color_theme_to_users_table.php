4ec8e8bac0a2d5008f92990c7b9cdf10f0ec3426
Pedro Soto <pedrosotto717@gmail.com>
2024-06-24 22:05:53 -0400

    Added profile_photo and color_theme

---
+++ database/migrations/2024_06_25_020253_add_profile_photo_and_color_theme_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('profile_photo')->nullable();
+                $table->string('color_theme')->nullable();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('profile_photo');
+                $table->dropColumn('color_theme');
+            });
+        });
+    }
+};
